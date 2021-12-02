dec4c2c3153b42dc334d92f74fa9400eb577640f
alex <jkaalexkei@gmail.com>
2021-12-02 17:42:28 -0500

    se actualizaron las vistas agregando información al pie de pagina

---
+++ plantillas/sitiogaseosaslacigarra/js/efectos.js
@@ -0,0 +1,35 @@
+// window.addEventListener('scroll',function(){
+//     let animacion = this.document.getElementById('subtitulo');
+//     let posicionobj = animacion.getBoundingClientRect().top;//se obtiene la posicion al hacer scroll
+
+//     let tamagnopantalla = window.innerHeight; //se obtiene tamaño de pantalla
+    
+//     if (posicionobj < tamagnopantalla){
+//         animacion.style.animation = 'moverin 1s ease-in';
+//     }
+
+// })
+
+// window.addEventListener('scroll',function(){
+//     let animacion = this.document.getElementById('nosotros');
+//     let posicionobj = animacion.getBoundingClientRect().top;//se obtiene la posicion al hacer scroll
+
+//     let tamagnopantalla = window.innerHeight; //se obtiene tamaño de pantalla
+    
+//     if (posicionobj < tamagnopantalla){
+//         animacion.style.animation = 'moverout ease-in-out 1s';
+//     }
+
+// })
+
+// window.addEventListener('scroll',function(){
+//     let animacion = this.document.getElementById('imagenobj');
+//     let posicionobj = animacion.getBoundingClientRect().top;//se obtiene la posicion al hacer scroll
+
+//     let tamagnopantalla = window.innerHeight / 2.5; //se obtiene tamaño de pantalla
+    
+//     if (posicionobj < tamagnopantalla){
+//         animacion.style.animation = 'moverout ease-in 1s';
+//     }
+
+// })
